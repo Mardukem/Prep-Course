ca3a04c3af63ef52e9066258baad5e16855d6bee
Mardukem <gs.emmanuel28@outlook.com>
2022-04-14 17:22:43 -0500

    Zeyn el pro

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'Soy Henry';
 const nuevoNum = 28;
 
 // Crea una variable booleana:
-const nuevoBool = true;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
